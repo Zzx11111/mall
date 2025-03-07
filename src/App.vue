91bfeb9d3ebbbab5fefa1a68fa3b08e70c9e08d5
zzx <38390941+Zzx11111@users.noreply.github.com>
2025-03-07 10:38:28 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,15 @@ export default {
   name: 'App',
   components:{
     MainTabBar
+  },
+  method:{
+    a() {
+      // 拉卡拉账号:djiwsd@lakala.com 密码:fjkwJKs=23
+       // 邦得数字账号:bangdeng234 密码:jerjiod334@#
+       // user:jkewe@lakala.com pass:sdkke@304
+       // user:dfjke@bonddigit.com pass:erl3934#
+       // user:jower@kinorsoft.com pass:wejk3*3
+    }
   }
 }
 </script>
